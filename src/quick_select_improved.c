f37514ce565112c7fb19280357a64fb917c6040b
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-06-26 18:37:08 +0900

    select_improved

--- src/quick_select_improved.c
+++ src/quick_select_improved.c
@@ -10,22 +10,34 @@ A[0], A[1], ..., A[n-1] の中でk+1番目に小さい値を返す関数
 ただし、Aの中身は書き換えてしまう。
 */
 int quick_select(int A[], int n, int k){
-  int i, j, pivot;
-
-// 先頭の要素をピボットとする
-  pivot = A[0];
-  for(i = j = 1; i < n; i++){
-    if(A[i] <= pivot){
-      int z = A[j];
-      A[j] = A[i];
-      A[i] = z;
-      j++;
-    }
-  }
-
-  if(j == k+1) return pivot;
-  else if(j < k+1) return quick_select(A+j, n-j, k-j);
-  else return quick_select(A+1, j-1, k);
+    int l ,r ,i ,pivot;
+    if(n==1)return A[0];
+    pivot = A[0];
+    r = n-1;
+    for(l=i=1 ; i<n ; i++){   //
+     if(A[i]<pivot){
+       int z = A[l];
+       A[l] = A[i];
+       A[i] = z;  
+       l++; 
+     }
+     }
+     for(i=n-1 ; i>0 ; i--){
+      if(A[i]>pivot){
+        int w = A[r];
+        A[r] = A[i];
+        A[i] = w;
+        r--;
+     }
+     }
+     
+     int q = A[0];
+     A[0] = A[l-1];
+     A[l-1] = q;
+ 
+     if(k<l-1) return quick_select(A,l-1,k);
+     else if(l<=k && k<=r) return pivot;
+     else return quick_select(A+r+1,n-r-1,k-r);
 }
 
 int main(){
