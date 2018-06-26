1794e7774057f37631b5048dbc372808081d9e99
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-06-26 18:34:42 +0900

    sort

--- src/quick_sort.c
+++ src/quick_sort.c
@@ -9,7 +9,23 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
-}
+	int i, j, pivot;
+	if(n<=1){return;}
+	 pivot = A[0];
+  for(i = j = 1; i < n; i++){
+    if(A[i] <= pivot){
+      int z = A[j];
+      A[j] = A[i];
+      A[i] = z;
+      j++;
+    }
+  }                                          //ピボットーj-1個のピボット以下の要素ー残り
+  int w = A[0];
+  A[0] = A[j-1];
+  A[j-1] = w;
+  quick_sort(A,j);
+  quick_sort(A+j,n-j);
+  }
 
 int main(){
   int i;
