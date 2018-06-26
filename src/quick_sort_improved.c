f9eab7d24e4c3f779ebdb88632c499966e9191d4
zentaro jinno <jinno.z.aa@m.titech.ac.jp>
2018-06-26 18:36:02 +0900

    sort_improved

--- src/quick_sort_improved.c
+++ src/quick_sort_improved.c
@@ -9,6 +9,32 @@ int A[N];
 A[0], A[1], ..., A[n-1] をソートして昇順に書き換える関数
 */
 void quick_sort(int A[], int n){
+    int l ,r ,i ,pivot;
+    if(n<=1){return;}
+    pivot = A[0];
+    r = n-1;
+    for(l=i=1 ; i<n ; i++){   //
+     if(A[i]<pivot){
+       int z = A[l];
+       A[l] = A[i];
+       A[i] = z;  
+       l++; 
+     }
+     }
+     for(i=n-1 ; i>0 ; i--){
+      if(A[i]>pivot){
+        int w = A[r];
+        A[r] = A[i];
+        A[i] = w;
+        r--;
+     }
+     }
+     
+     int q = A[0];
+     A[0] = A[l-1];
+     A[l-1] = q;
+     quick_sort(A,l);
+     quick_sort(A+r+1,n-r-1);   
 }
 
 int main(){
